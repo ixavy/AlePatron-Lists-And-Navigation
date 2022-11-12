e7b8bf62ed786f935f82ddc4a24b72bc1914dccf
ixavy <ict@ixavy.com>
2022-11-12 20:03:53 +0100

    First iXavy commit

---
+++ Lists and Navigation/Lists_and_NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lists_and_NavigationApp.swift
+//  Lists and Navigation
+//
+//  Created by iXavy on 27/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Lists_and_NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# AlePatron-Lists-and-Navigation
